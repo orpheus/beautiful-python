0edf079ff3bc29db04e1f55723ea60cdac0cb208
ohryan <ryanachacon@gmail.com>
2022-07-14 23:32:26 -0700

    dicts

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 Beautiful is better than Ugly.
 """
 import io
+from collections import defaultdict
 from functools import partial
 
 """
@@ -139,4 +140,93 @@ def no_break(seq):
 print(no_break('x'))
 print(no_break('n'))
 
+"""
+Dictionaries
 
+"There are two types of people in this world:
+Those who've mastered dictionaries and total goobs." - Raymond Hettinger
+"""
+
+"""
+Looping over dictionary keys
+"""
+d = {'ryan': 'blue', 'erik': 'green', 'shane': 'red'}
+for k in d:
+    print(k)
+
+"""
+You cannot mutate an object while iterating over it, if you do, 
+"you're living in a tatue of sin".
+
+d.keys() in python2 would make a copy of the keys into a list that you
+could iterate over and mutate the original object.
+
+In python3, you use `list(some_dict)` to return a copy of the keys.
+"""
+for k in list(d):
+    if k.startswith('r'):
+        del d[k]
+
+"""
+Looping over keys and values at the same time:
+"""
+d = {'ryan': 'blue', 'erik': 'green', 'shane': 'red'}
+for k in d:
+    print(k, '->', d[k])  # d[k]: Not fast. Has to re-hash every key and do a look-up on it
+
+for k, v in d.items():  # .items(): provides an iterable set on a view of the items
+    print(k, '-> ', v)
+
+"""
+Construct a Dictionary From Pairs:
+    zip again!
+        see how beautifully the python functions work together?
+"""
+colors = ['red', 'blue', 'green']
+names = ['ryan', 'erik', 'luna', 'drew']
+zip_dict = dict(zip(colors, names))
+print('dict(zip( ', zip_dict)
+
+# noinspection PyTypeChecker
+enum_dict = dict(enumerate(names))
+print('dict(enumerate( ', enum_dict)
+
+"""
+Counting with Dictionaries:
+    Do not teach beginners this. Have them start off with looping 
+    and learning the .get(key, defaultValue) fn or checking for keys first
+"""
+colors = ['red', 'blue', 'green', 'red', 'red', 'green']
+d = defaultdict(int)  # `int` can be called with 0 arguments results in 0
+for c in colors:
+    d[c] += 1
+print(dict(d))  # defaultdict has to be converted back
+
+"""
+Grouping with dictionaries
+"""
+# Beginner Way
+names = ['ryan', 'erik', 'luna', 'drew', 'jordan', 'bri', 'molly', 'sarah']
+d = {}
+for n in names:
+    l_key = len(n)
+    if l_key not in d:
+        d[l_key] = []
+    d[l_key].append(n)
+print(d)
+
+# setdefault
+names = ['ryan', 'erik', 'luna', 'drew', 'jordan', 'bri', 'molly', 'sarah']
+d = {}
+for n in names:
+    key = len(n)
+    d.setdefault(key, []).append(n)  # returns value at key with default set to [] if None
+print(d)
+
+# defaultdict
+names = ['ryan', 'erik', 'luna', 'drew', 'jordan', 'bri', 'molly', 'sarah']
+d = defaultdict(list)  # sets the default value for any key to a list
+for n in names:
+    key = len(n)
+    d[key].append(n)
+print(dict(d))
